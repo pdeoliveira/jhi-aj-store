212e015475bc21cafd50743433aa96377d694123
Pedro C. Oliveira <pedro.c.oliveira@accenture.com>
2020-09-15 08:25:17 -0300

    generated online store entity model

---
+++ src/main/java/com/accenture/ecommerce/store/repository/InvoiceRepository.java
@@ -0,0 +1,14 @@
+package com.accenture.ecommerce.store.repository;
+
+import com.accenture.ecommerce.store.domain.Invoice;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the Invoice entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+}
